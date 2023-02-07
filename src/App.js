d8a3879c8246cc75d268fe67d53648bfee63bc04
Nitin <nitinkhokher999@gmail.com>
2023-02-07 16:48:14 +0530

    added few components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { useState } from "react";
 
 import "./App.css";
-import AddUserForm from "./Components/AddUserForm/AddUserForm";
-import Users from "./Components/UserList/Users";
+import AddUser from "./Components/Users/AddUser";
 
-const demousers = [
-  { key: 0.1785, name: "robert", age: 42 },
-  { key: 0.2123, name: "Rohit", age: 22 },
-];
 
 function App() {
-  const [usersList, setUsersList] = useState(demousers);
-
-  const addUserHandler = (newUser) => {
-    setUsersList((prevState) => {
-      return [newUser, ...prevState];
-    });
-  };
+  
 
   return (
     <div className="App">
-      <AddUserForm fun={addUserHandler} />
-      <Users users={usersList}  />
+      <AddUser />
     </div>
   );
 }
